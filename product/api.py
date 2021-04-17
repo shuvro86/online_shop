0c14fc55b19773cf7d031517c56b21d6f2a655fc
mdrajibkhan <68429876+mdrajibkhan@users.noreply.github.com>
2021-04-18 01:35:35 +0600

    Update api.py

--- product/api.py
+++ product/api.py
@@ -11,7 +11,7 @@ api = Api(app)
 class Product(Resource):
     def get(self):
         return {
-            'products': ['Ice cream', 'Chocolate', 'Fruit', 'Eggs', 'Subroto']
+            'products': ['Ice cream', 'Chocolate', 'Fruit', 'Eggs', 'Subroto hungry']
         }
 
 # Create routes
