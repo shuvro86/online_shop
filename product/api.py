a3cb165588e70f701c001bab335b8aecb169061b
page-cloud <79392258+page-cloud@users.noreply.github.com>
2023-08-07 23:45:36 +0600

    Update api.py

--- product/api.py
+++ product/api.py
@@ -11,7 +11,7 @@ api = Api(app)
 class Product(Resource):
     def get(self):
         return {
-            'products': ['Ice cream', 'Chocolate']        }
+            'products': ['Ice cream', 'Chocolate', 'Apple']        }
 
 # Create routes
 api.add_resource(Product, '/')
